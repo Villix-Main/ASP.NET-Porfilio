dacd358955519856ea6cf84f32b317eb4645dccd
Dominic <Martinez>
2021-01-04 09:26:47 -0500

    Added project for dependecy injection

---
+++ src/ASPNETFundamentals/DependencyInjection/Controllers/PeopleController.cs
@@ -0,0 +1,27 @@
+﻿using DependencyInjection.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private Person _person;
+
+        public PeopleController(Person person)
+        {
+            _person = person;
+        }
+        
+        [HttpGet]
+        public ActionResult<Person> GetPerson() => Ok(_person);
+    }
+}
---
+++ src/ASPNETFundamentals/DependencyInjection/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Models
+{
+    public class Person
+    {
+        public int Id { get; init; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public Person()
+        {
+            Id = new Random().Next(1, 15);
+        }
+    }
+}
